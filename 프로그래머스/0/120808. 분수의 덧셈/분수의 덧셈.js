588fabb4e3373bb61c6451b6df9af409aab07877
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-08-29 16:29:29 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,19 +1,10 @@
+function GCD (a, b) {
+    return (a % b) ? GCD(b, a % b) : b;
+}
+
 function solution(numer1, denom1, numer2, denom2) {
-    var answer = [];
-    var tmp = denom1;
-    numer1 *= denom2;
-    denom1 *= denom2;
-    numer2 *= tmp;
-    denom2 *= tmp;
-    var res_numer = numer1 + numer2;
-    var res_denom = denom1;
-    for (var i=2; i<=res_denom; i++) {
-        while (res_denom % i == 0 && res_numer % i == 0) {
-          res_denom /= i;
-          res_numer /= i;
-        }
-    }
-    answer.push(res_numer);
-    answer.push(res_denom);
-    return answer;
+    var numer = numer1 * denom2 + numer2 * denom1;
+    var denom = denom1 * denom2;
+    var gcd = GCD(numer, denom);
+    return [numer / gcd, denom / gcd];
 }
