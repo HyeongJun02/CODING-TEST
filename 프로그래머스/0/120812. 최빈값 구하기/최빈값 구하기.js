6dcdadd3daa47ece0f45811f30f9c77a0f181bfb
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-02 09:49:31 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,13 +1,13 @@
 function solution(array) {
-    var cnt = Array(1000).fill(0);
-    var max = 0;
-    var answer = 0;
-    for (var e of array) {
-        cnt[e]++;
-        if (cnt[e] > max) {
-            max = cnt[e];
-            answer = e;
-        } else if (cnt[e] == max) answer = -1;
+    const freq = new Map();
+    let max = 0;
+    
+    for (const e of array) {
+        const count = (freq.get(e) || 0) + 1;
+        freq.set(e, count);
+        max = (count > max) ? count : max;
     }
-    return answer;
+    
+    const c = [...freq. entries()].filter(([_, v]) => v == max);
+    return c.length == 1 ? c[0][0] : -1;
 }
