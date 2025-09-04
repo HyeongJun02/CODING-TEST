bac9fa532a5272f64d9032c64f4bbc989713bdc6
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-04 15:30:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.74 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,13 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) return participant[i];
+    let hashed = []
+    participant.forEach(entry => {
+        hashed[entry] = hashed[entry] ? hashed[entry] + 1 : 1        
+    })
+    completion.forEach(entry => {
+        hashed[entry] = hashed[entry] - 1
+    })
+
+    for (var key in hashed) {
+        if (hashed[key] >= 1) return key
     }
 }
