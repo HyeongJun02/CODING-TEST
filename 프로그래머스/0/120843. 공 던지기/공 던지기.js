8a2c50c07fc55e613f826ae1601de8c4739acac7
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-08 09:56:04 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,9 +1,1 @@
-function solution(numbers, k) {
-    let now = -1;
-    let leng = numbers.length
-    while (k--) {
-        now += 2;
-        now = (now > leng) ? (now % leng) : now;
-    }
-    return now;
-}
+const solution = (numbers, k) => numbers[((k - 1) * 2) % numbers.length];
