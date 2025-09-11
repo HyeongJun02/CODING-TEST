2bd2cd37788cd81b841f9bb563158e4714bcc8d1
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-11 10:11:10 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,7 +1,1 @@
-function solution(n, numlist) {
-    var answer = [];
-    for (let i = 0; i < numlist.length; i++) {
-        if (numlist[i] % n == 0) answer.push(numlist[i]);
-    }
-    return answer;
-}
+const solution = (n, numlist) => numlist.filter((e) => e % n == 0);
