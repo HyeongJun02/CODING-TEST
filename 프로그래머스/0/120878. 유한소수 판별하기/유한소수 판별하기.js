62f1f8955825607420a1aae541ef300abeb1d56b
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-16 14:12:14 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,32 +1,12 @@
 const GCD = (a, b) => ((a % b) ? GCD(b, a % b) : b);
 
-function isPrime(num) {
-  if(num === 2) {
-    return true;
-  }
-  for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
-    if(num % i === 0){
-      return false; 
-    }
-  }
-  return true; 
-}
-
 function solution(a, b) {
-    function check(b) {
-        for (let i = 2; i <= b; i++) {
-            if (!isPrime(i)) continue;
-            if (b % i == 0) {
-                if (i == 2 || i == 5) {
-                    continue;
-                } else
-                    return 2;
-            }
-        }
-        return 1;
-    }
     let g = GCD(a, b);
     a /= g;
     b /= g;
-    return check(b);
+    
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
+    
+    return b == 1 ? 1 : 2;
 }
