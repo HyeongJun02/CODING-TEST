1b7c510e0621e22b6f14e7044fa0346e684114c7
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-18 14:00:22 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,12 +1,3 @@
 function solution(A, B) {
-    let cnt = 0;
-    for (let i = 0; i < A.length; i++) {
-        if (A == B) return cnt;
-        A = A.split("");
-        let temp = A.pop();
-        A.unshift(temp);
-        A = A.join("");
-        cnt++;
-    }
-    return -1;
+    return (B + B).indexOf(A);
 }
