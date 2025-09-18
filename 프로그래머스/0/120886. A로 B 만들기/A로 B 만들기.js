171b0a0c0e375375ebd2df57dd366cd955463b76
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-18 13:37:34 +0900

    [level 0] Title: A로 B 만들기, Time: 0.82 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,13 +1,3 @@
 function solution(before, after) {
-    let bef = Array(26).fill(0);
-    let aft = Array(26).fill(0);
-    if (before.length != after.length) return 0;
-    for (let i = 0; i < before.length; i++) {
-        bef[before[i].charCodeAt() - 97]++;
-        aft[after[i].charCodeAt() - 97]++;
-    }
-    for (let i = 0; i < 26; i++) {
-        if (bef[i] != aft[i]) return 0;
-    }
-    return 1;
+    return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0
 }
