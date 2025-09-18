9f7e064bf2de8694acd4309fb55d32b76fb42e18
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-18 14:40:25 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,7 +1,6 @@
 function solution(common) {
     let type; // 0: 공차수열, 1: 공비수열
-    let leng = common.length;
-    type = (common[1] - common[0] == common[leng - 1] - common[leng - 2]) ? 0 : 1;
+    type = (common[1] - common[0] == common[2] - common[1]) ? 0 : 1;
     let weight = type ? (common[1] / common[0]) : (common[1] - common[0]);
-    return type ? (common[leng - 1] * weight) : (common[leng - 1] + weight);
+    return type ? (common[common.length - 1] * weight) : (common[common.length - 1] + weight);
 }
