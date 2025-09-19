e7880d6c466bfce713232ca510fa1b1b35f0debf
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-09-19 09:58:44 +0900

    [level 2] Title: 프로세스, Time: 2.03 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,14 +1,14 @@
 function solution(priorities, location) {
-  let queue = priorities.map((p, i) => [p, i]);
-  let order = 0;
-
-  while (queue.length) {
-    const [priority, idx] = queue.shift();
-    if (queue.some(([p]) => p > priority)) {
-      queue.push([priority, idx]);
-    } else {
-      order++;
-      if (idx === location) return order;
+    
+    let queue = priorities.map((p, i) => [p, i]);
+    let order = 0;
+    
+    while (queue.length) {
+        const [priority, index] = queue.shift();
+        if (queue.some(([p]) => p > priority)) queue.push([priority, index]);
+        else {
+            order++;
+            if (index == location) return order;
+        }
     }
-  }
 }
