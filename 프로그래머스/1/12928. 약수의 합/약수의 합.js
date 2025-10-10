49136a544e3ee27c9f9c56faa11f8289ec9f9352
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-10-10 10:32:38 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let result = 0;
+    let index = 1;
+    while (index <= Math.sqrt(n)) {
+        if (n % index === 0) {
+            result += index;
+            if (n / index !== index) result += (n / index);
+        }
+        index++;
+    }
+    return result;
+}
