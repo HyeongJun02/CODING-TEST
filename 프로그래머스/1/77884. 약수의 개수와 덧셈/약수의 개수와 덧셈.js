38a2b213bfba8239d587d5911e21ce081b41c268
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-10-20 17:49:00 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,11 @@
-function check(n) {
-    let cnt = 0;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (i == Math.sqrt(n)) cnt++;
-        if (n % i == 0) cnt += 2;
-    }
-    return cnt;
-}
-
 function solution(left, right) {
-    let result = 0;
+    var answer = 0;
     for (let i = left; i <= right; i++) {
-        result += (check(i) % 2 ? -i : +i);
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
     }
-    return result;
+    return answer;
 }
