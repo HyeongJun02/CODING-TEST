0df7db62b433c2fc1d34c1e03f891e66aa33b5af
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-10-23 11:22:06 +0900

    [level 2] Title: 오픈채팅방, Time: 345.78 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,35 +1,26 @@
 function solution(record) {
-    let log = [];
-    let user = new Map();
+    const log = [];
+    const user = new Map();
+    const state = {
+        'Enter': '님이 들어왔습니다.',
+        'Leave': '님이 나갔습니다.'
+    }
+    
+    let result = [];
     
     for (let str of record) {
         // 문자열 분리
         str = str.split(" ");
-        
-        // command, id, name 분리
         let [command, id, name] = [str[0], str[1], str[2]];
-        // console.log("String: ", command, id, name ? name : " ");
-        
         // Enter, Leave인 경우 log에 추가
         if (command !== "Change") log.push([command, id]);
-        
         // Enter, Change인 경우 닉네임 설정
-        if (command !== "Leave") {
-            user[id] = name;
-            // console.log("user[", id, "]: ", user[id]);
-        }
+        if (command !== "Leave") user[id] = name;
     }
     
-    // console.log("log: ", log);
-    
-    let result = [];
     // log 기반 메시지 생성
     for (let l of log) {
-        if (l[0] === "Enter") {
-            result.push(user[l[1]] + "님이 들어왔습니다.");
-        } else {
-            result.push(user[l[1]] + "님이 나갔습니다.");
-        }
+        result.push(user[l[1]] + state[l[0]]);
     }
     return result;
 }
