835ad901e0a9dfa34723434839b2b5b09e8a89ce
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-10-23 14:18:15 +0900

    [level 3] Title: 베스트앨범, Time: 0.35 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -9,14 +9,22 @@ function solution(genres, plays) {
         sortedMap[genres[i]].push([i, plays[i]]);
         playSum[genres[i]] = (playSum[genres[i]] || 0) + plays[i];
     }
+    
+    // console.log('playSum: ', playSum);
 
     // 장르별 총 재생수 기준으로 정렬
+    // playSum -> sortedPlays
     const sortedPlays = Object.entries(playSum).sort((a, b) => b[1] - a[1]);
 
+    // console.log('sortedPlays: ', sortedPlays);
+    
     // 각 장르 내에서 재생수 기준으로 정렬
+    // sortedMap
     for (const key in sortedMap) {
         sortedMap[key].sort((a, b) => b[1] - a[1]);
     }
+    
+    // console.log('sortedMap:', sortedMap);
 
     // 정렬된 장르 순서대로 최대 2곡씩 뽑기
     const answer = [];
