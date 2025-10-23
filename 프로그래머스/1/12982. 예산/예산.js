881c0f394a60d8a20a8180e5711140a602e83fcc
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-10-23 09:31:48 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,9 +1,5 @@
 function solution(d, budget) {
-    d.sort((a, b) => a - b);
-    let total = 0;
-    for (var i = 0; i < d.length; i++) {
-        total += d[i];
-        if (total > budget) break;
-    }
-    return i;
+    return d.sort((a, b) => a - b).reduce((count, price) => {
+        return count + ((budget -= price) >= 0);
+    }, 0);
 }
