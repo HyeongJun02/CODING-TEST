c53fa77432d37b011431223e683ab0aa91382bbe
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-11-03 11:35:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,14 @@
+function solution(n, arr1, arr2) {
+    let answer = [];
+    for (let i = 0; i < n; i++) {
+        let bin1 = arr1[i].toString(2).padStart(n, '0');
+        let bin2 = arr2[i].toString(2).padStart(n, '0');
+        let temp = "";
+        for (let j = 0; j < n; j++) {
+            if (bin1[j] == "0" && bin2[j] == "0") temp += " ";
+            else temp += "#";
+        }
+        answer.push(temp);
+    }
+    return answer;
+}
