461951694542948786e3afd16ac1c831ceadd991
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-11-18 16:30:49 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    const answer = Array.from({ length: n }, () => Array(n).fill(0));
+
+    let num = 1;
+    let top = 0,
+        bottom = n - 1,
+        left = 0,
+        right = n - 1;
+
+    while (num <= n * n) {
+        // 오른쪽 이동
+        for (let i = left; i <= right; i++) answer[top][i] = num++;
+        top++;
+
+        // 아래로 이동
+        for (let i = top; i <= bottom; i++) answer[i][right] = num++;
+        right--;
+        
+        // 왼쪽 이동
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) answer[bottom][i] = num++;
+            bottom--;
+        }
+
+        // 위로 이동
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) answer[i][left] = num++;
+            left++;
+        }
+    }
+
+    return answer;
+}
