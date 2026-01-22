695ade396c171ffddb2269d65ecdda404a1f1c20
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2026-01-22 11:45:50 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.72 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,28 +1,11 @@
 function solution(keymap, targets) {
-    const alpha = new Array(26).fill(500);
-    
-    for (let i = 0; i < 26; i++) {
-        for (let key of keymap) {
-            let index = key.indexOf(String.fromCharCode(65 + i));
-            if (index == -1) continue;
-            alpha[i] = alpha[i] < (index + 1) ? alpha[i] : (index + 1);
-        }
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
     }
-    
-    let result = [];
-    
-    for (let target of targets) {
-        let count = 0;
-        for (let ch of target.split("")) {
-            let code = ch.charCodeAt(0);
-            if (alpha[code - 65] === 500) {
-                count = -1;
-                break;
-            }
-            count += alpha[code - 65];
-        }
-        result.push(count);
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
     }
-    
-    return result;
+    return answer;
 }
