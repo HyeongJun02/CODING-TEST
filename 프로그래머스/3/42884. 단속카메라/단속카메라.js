d2dd99a2fb0d8cc42851ef9b3441570c3030360c
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2026-01-22 15:52:43 +0900

    [level 3] Title: 단속카메라, Time: 21.76 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,15 +1,12 @@
 function solution(routes) {
     routes.sort((a, b) => a[1] - b[1]);
     let lastCamera = -30001;
-    // let lastCamera = routes[0][1];
     let result = 0;
     
     for (let route of routes) {
         if (lastCamera >= route[0]) continue;
-        else {
-            lastCamera = route[1];
-            result++;
-        }
+        lastCamera = route[1];
+        result++;
     }
     
     return result;
