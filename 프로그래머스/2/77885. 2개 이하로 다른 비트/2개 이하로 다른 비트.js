4942a3319fcda53a4369e5ed36dd611d7eb835e2
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2026-03-20 15:21:54 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 379.49 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -0,0 +1,31 @@
+// function solution(numbers) {
+//     return numbers.map(v => {
+//         if (v % 2 === 0) return v + 1;
+//         let lastZero = ~v & (v + 1);
+//         return v + lastZero - (lastZero >> 1);
+//     });
+// }
+
+function solution(numbers) {
+    let result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i].toString(2).split("");
+        number.unshift("0");
+        let saveIndex;
+        for (let j = number.length - 1; j >= 0; j--) {
+            if (number[j] === "0") {
+                number[j] = "1";
+                saveIndex = j + 1;
+                break;
+            }
+        }
+        for (let k = saveIndex; k < number.length; k++) {
+            if (number[k] === "1") {
+                number[k] = "0";
+                break;
+            }
+        }
+        result.push(parseInt(number.join(""), 2));
+    }
+    return result;
+}
